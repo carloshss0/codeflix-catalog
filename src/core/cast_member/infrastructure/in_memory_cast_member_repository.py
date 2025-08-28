0d19fd4839d8c85cd4d32b0a8ed45ee616b655ed
Carlos <carloshss97@gmail.com>
2025-08-28 15:31:02 -0300

    refactor: guarantee that we're only saving and getting a copy of CastMember object

--- src/core/cast_member/infrastructure/in_memory_cast_member_repository.py
+++ src/core/cast_member/infrastructure/in_memory_cast_member_repository.py
@@ -1,3 +1,4 @@
+import copy
 from uuid import UUID
 
 from src.core.cast_member.domain.cast_member import CastMember
@@ -10,12 +11,12 @@ class InMemoryCastMemberRepository(CastMemberRepository):
         self.cast_members = cast_members or []
 
     def save(self, cast_member) -> None:
-        self.cast_members.append(cast_member)
+        self.cast_members.append(copy.deepcopy(cast_member))
 
     def get_by_id(self, id: UUID) -> CastMember:
         for cast_member in self.cast_members:
             if cast_member.id == id:
-                return cast_member
+                return copy.deepcopy(cast_member)
         return None
 
     def delete(self, id: UUID) -> None:
@@ -29,7 +30,7 @@ class InMemoryCastMemberRepository(CastMemberRepository):
         old_cast_member = self.get_by_id(cast_member.id)
         if old_cast_member:
             self.cast_members.remove(old_cast_member)
-            self.cast_members.append(cast_member)
+            self.cast_members.append(copy.deepcopy(cast_member))
 
     def list(self) -> list[CastMember]:
         return self.cast_members
